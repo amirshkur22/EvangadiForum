e15712ea3e7ab3557feb8efd39c9061f9880db64
amirshkur22 <amirshkurhu@gmail.com>
2024-06-10 21:24:58 +0300

    last commit

--- FrontEnd/src/Components/banner/Banner.jsx
+++ FrontEnd/src/Components/banner/Banner.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import bannerStyle from "./banner.module.css";
 import { useNavigate } from "react-router-dom";
+import useCountUp from "../useCountUp/useCountUp";
 
 const Banner = () => {
-  const navigate=useNavigate()
+  const registeredMembers = useCountUp(31000, 2000); // 2 seconds
+  const scholarshipWinners = useCountUp(1500, 2000); // 2 seconds
+  const activeStudents = useCountUp(2500, 2000);
+  const navigate = useNavigate();
   return (
     <>
       <section className={bannerStyle.bannerContainer}>
@@ -17,29 +21,41 @@ const Banner = () => {
             <span>join the new digital era.</span>
           </h4>
           <div className={bannerStyle.buttonContainer}>
-            <button type="button" onClick={()=>navigate('/signup')}>create account</button>
-            <button type="button" onClick={()=>navigate('/login')}>sign in</button>
+            <button type="button" onClick={() => navigate("/signup")}>
+              create account
+            </button>
+            <button type="button" onClick={() => navigate("/login")}>
+              sign in
+            </button>
           </div>
         </div>
       </section>
       <section className={`${bannerStyle.lowerBanner} container`}>
-        <div className="row">
-          <div className="col">
-            <h1>31000+</h1>
-            <h3>Registered <br /> Members</h3>
-            <p>These are the individuals who have faith in the vision and became part of the movement.</p>
+        <div className={bannerStyle.stats}>
+          <div className={bannerStyle.stat}>
+            <h2>{registeredMembers}+</h2>
+            <p>Registered Members</p>
+            <p className="col-description">
+              These are the individuals who have faith in the vision and became
+              part of the movement.
+            </p>
           </div>
-          <div className="col">
-            <h1>1500 +</h1>
-            <h3>Scholarship Winners </h3>
-            <p>We are only as strong as our weakest link. We try to include everyone in the journey.</p>
+          <div className={bannerStyle.stat}>
+            <h2>{scholarshipWinners}+</h2>
+            <p>Scholarship Winners</p>
+            <p className="col-description">
+              We are only as strong as our weakest link. We try to include
+              everyone in the journey.
+            </p>
           </div>
-          <div className="col">
-            <h1>2500 +</h1>
-            <h3>Active Students</h3>
-            <p>These are students trying to acquire the necessary digital skills to compete globally.</p>
+          <div className={bannerStyle.stat}>
+            <h2>{activeStudents}+</h2>
+            <p>Active Students</p>
+            <p className="col-description">
+              These are students trying to acquire the necessary digital skills
+              to compete globally.
+            </p>
           </div>
-          
         </div>
       </section>
     </>
